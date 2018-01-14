2cb1f0d03877006b0d0f527ab1ae784324da355e
Mary Thielking <mthielki@mit.edu>
2018-01-14 16:21:38 -0500

    Header displays

---
+++ react/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from './Header.jsx';
+
+export default class App extends Component {
+    render(){
+      return (
+        <div>
+          <Header />
+          <div>Hello World!</div>
+        </div>
+      );
+    }
+}
