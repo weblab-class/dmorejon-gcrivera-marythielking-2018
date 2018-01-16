7ed458cf888ef363c3604f9a66e1db667ff02030
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-16 14:26:55 -0500

    fixed user schema

--- models/user.js
+++ models/user.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 var userModel = mongoose.model('User', mongoose.Schema({
   fbid: {type: Number, required: true, unique: true},
   displayname: {type: String, required: true},
-  username: {type: String, required: true}, // TODO: possibly remove required
-  email: {type: String, required: true} // TODO: possibly remove required
+  username: {type: String, required: true, unique: true}, // TODO: possibly remove required
+  email: {type: String, required: true, unique: true} // TODO: possibly remove required
 }));
 
 const user = (userModel) => {
