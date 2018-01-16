129461bbe14e3a93eae9dad21a91a98e722ed378
Mary Thielking <mthielki@mit.edu>
2018-01-15 20:10:53 -0500

    Updated map view

--- react/LeafletMap.jsx
+++ react/LeafletMap.jsx
@@ -7,13 +7,9 @@ export default class LeafletMap extends Component {
     var map = this.map = L.map(ReactDOM.findDOMNode(this), {
       center: [42.3601, -71.0942],
       zoom: 16,
-      layers: [
-        L.tileLayer(
-          'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-          {attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'})
-      ],
-      attributionControl: false,
     });
+    var layer = new L.StamenTileLayer("terrain");
+    map.addLayer(layer);
   }
 
   componentWillUnmount() {
