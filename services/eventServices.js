273d5f753b468a1c412dcbbd75a3b461d0a3eb7f
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-18 13:29:45 -0500

    added event service calls

--- services/eventServices.js
+++ services/eventServices.js
@@ -3,6 +3,31 @@ const BASE_URL = 'http://localhost:3000/event';
 var request = require('request-promise-native');
 
 export default {
+
+  info : (eventid) => {
+    return request({
+      uri : BASE_URL + `/${eventid}`,
+      method: 'GET',
+      json : true
+    });
+  },
+
+  getAllByUser : () => {
+    return request({
+      uri : BASE_URL + '/user',
+      method: 'GET',
+      json : true
+    });
+  },
+
+  getAllByGreenspace : (greenspaceid) => {
+    return request({
+      uri : BASE_URL + `/greenspace/${greenspaceid}`,
+      method: 'GET',
+      json : true
+    });
+  },
+
   create : (name, description, greenspace, starttime, endtime, participants) => {
     return request({
       uri : BASE_URL,
@@ -18,4 +43,47 @@ export default {
       }
     });
   },
+
+  edit : (eventid, name, description, greenspace, starttime, endtime, participants) => {
+    return request({
+      uri : BASE_URL + `/${eventid}`,
+      method: 'PUT',
+      json : true,
+      body : {
+        name,
+        description,
+        greenspace,
+        starttime,
+        endtime,
+        participants,
+      }
+    });
+  },
+
+  join : (eventid) => {
+    return request({
+      uri : BASE_URL + `/join/${eventid}`,
+      method: 'PUT',
+      json : true
+    });
+  },
+
+  leave : (eventid, target) => {
+    return request({
+      uri : BASE_URL + `/leave/${eventid}`,
+      method: 'PUT',
+      json : true,
+      body : {
+        target,
+      }
+    });
+  },
+
+  delete : (eventid) => {
+    return request({
+      uri : BASE_URL + `/${eventid}`,
+      method: 'DELETE',
+      json : true
+    });
+  }
 }
