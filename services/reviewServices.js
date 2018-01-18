eacaac20bf7fac2f6ee6bee168a88de6df4a13a7
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-18 13:35:53 -0500

    review service calls

--- services/reviewServices.js
+++ services/reviewServices.js
@@ -3,6 +3,23 @@ const BASE_URL = 'http://localhost:3000/review';
 var request = require('request-promise-native');
 
 export default {
+
+  getAllByUser : (greenspaceid) => {
+    return request({
+      uri : BASE_URL + '/user',
+      method: 'GET',
+      json : true
+    });
+  },
+
+  getAllByGreenspace : (greenspaceid) => {
+    return request({
+      uri : BASE_URL + `/greenspace/${greenspaceid}`,
+      method: 'GET',
+      json : true
+    });
+  },
+
   create : (name, location) => {
     return request({
       uri : BASE_URL,
@@ -11,6 +28,17 @@ export default {
       body : {
         name,
         location,
+      }
+    });
+  },
+
+  delete : (greenspace) => {
+    return request({
+      uri : BASE_URL,
+      method: 'DELETE',
+      json : true,
+      body : {
+        greenspace,
       }
     });
   },
