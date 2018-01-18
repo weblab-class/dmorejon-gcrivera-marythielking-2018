cfc119bea3e9867ed0682cb42b4c72286b913850
Mary Thielking <mthielki@mit.edu>
2018-01-18 00:23:30 -0500

    Event list scrolls

--- react/Pages/GreenspaceInfo.jsx
+++ react/Pages/GreenspaceInfo.jsx
@@ -11,7 +11,9 @@ class GreenspaceInfo extends Component {
       lat: props.params.lat,
       lng: props.params.lng,
     };
+
     this.renderEvent = this.renderEvent.bind(this);
+    this.renderStar = this.renderStar.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
@@ -31,6 +33,17 @@ class GreenspaceInfo extends Component {
     >{name}</Link>);
   }
 
+  renderStar(s) {
+    const rating = 3.5;
+    if (s < rating) {
+      return (<FontAwesome name="star" key={s} />);
+    } else if (s - rating === 0.5) {
+      return (<FontAwesome name="star-half-o" key={s} />);
+    } else {
+      return (<FontAwesome name="star-o" key={s} />);
+    }
+  }
+
   render(){
     const { lat, lng } = this.state;
     const dummyEvents = [
@@ -42,12 +55,22 @@ class GreenspaceInfo extends Component {
       { name: "Event 6" },
       { name: "Event 7" },
       { name: "Event 8" },
+      { name: "Event 9" },
+      { name: "Event 10" },
+      { name: "Event 11" },
+      { name: "Event 12" },
+      { name: "Event 13" },
+      { name: "Event 14" },
+      { name: "Event 15" },
+      { name: "Event 16" },
     ]
     const events = dummyEvents.map((e) => this.renderEvent(e));
+    const stars = [1,2,3,4,5].map((s) => this.renderStar(s));
 
     return (
       <Sidebar>
-        <h1>Greenspace Info</h1>
+        <h1>Briggs Field</h1>
+        <div className="rating-stars">{stars}</div>
         <div className="list-items">{events}</div>
         <Link to={`/map/${lat},${lng}/event/create`} id="add-event">
           <FontAwesome name="plus-square-o" size="2x" id="add-event-icon" />
