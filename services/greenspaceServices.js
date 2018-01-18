b3cc54f6528184b320c144729f7059e16917ba02
Mary Thielking <mthielki@mit.edu>
2018-01-18 13:27:45 -0500

    Greenspace services

--- services/greenspaceServices.js
+++ services/greenspaceServices.js
@@ -3,15 +3,31 @@ const BASE_URL = 'http://localhost:3000/greenspace';
 var request = require('request-promise-native');
 
 export default {
+  info : (id) => {
+    return request({
+      uri: BASE_URL + `/${id}`,
+      method: 'GET',
+      json: true,
+    })
+  },
+
+  getAll : (topLeft, bottomRight) => {
+    return request({
+      uri: BASE_URL + `/${topLeft.lng}/${bottomRight.lng}/${bottomRight.lat}/${topLeft.lat}`,
+      method: 'GET',
+      json: true,
+    })
+  }
+
   create : (name, location) => {
     return request({
-      uri : BASE_URL,
+      uri: BASE_URL,
       method: 'POST',
-      json : true,
-      body : {
+      json: true,
+      body: {
         name,
         location,
-      }
+      },
     });
   },
 }
