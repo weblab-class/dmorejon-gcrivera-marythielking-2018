0fe59698503fea5c9ec9fca5bfa5700f3c4f4c3a
Mary Thielking <mthielki@mit.edu>
2018-01-21 13:13:33 -0500

    Added back buttons in sidebar

--- react/Pages/EventView.jsx
+++ react/Pages/EventView.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router';
+import FontAwesome from 'react-fontawesome';
 import Sidebar from '../Components/Sidebar.jsx';
 import eventServices from '../../services/eventServices.js';
 
@@ -33,9 +35,12 @@ class EventView extends Component {
       endDate = endtime.substring(0,10);
       endHour = endtime.substring(11,16);
     }
-    
+
     return (
       <Sidebar>
+        <Link to={`/map/${this.props.params.gid}/${window.location.search}`} id="back-button">
+          <FontAwesome name="chevron-left" size="2x" id="back-button-icon" />
+        </Link>
         <h1 className="section-header">{name}</h1>
         <div>{description}</div>
         <div>{startDate} {startHour} to {endDate} {endHour}</div>
@@ -44,4 +49,4 @@ class EventView extends Component {
   }
 }
 
-export default EventView;
+export default withRouter(EventView);
