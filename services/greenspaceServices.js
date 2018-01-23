41832cc5429404e703dffe7cf81f7143e3301df0
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-22 23:42:46 -0500

    preparing deploy

--- services/greenspaceServices.js
+++ services/greenspaceServices.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3000/greenspace'
-// const BASE_URL = 'https://greenspace2018.herokuapp.com/greenspace';
+// const BASE_URL = 'http://localhost:3000/greenspace'
+const BASE_URL = 'https://greenspace2018.herokuapp.com/greenspace';
 
 var request = require('request-promise-native');
 
