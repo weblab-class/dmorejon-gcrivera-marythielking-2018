ee5665f2a0a1edfec8adf89f0d7c9184a670147a
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-22 23:42:46 -0500

    preparing deploy

--- services/tagServices.js
+++ services/tagServices.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3000/tag'
-// const BASE_URL = 'https://greenspace2018.herokuapp.com/tag';
+// const BASE_URL = 'http://localhost:3000/tag'
+const BASE_URL = 'https://greenspace2018.herokuapp.com/tag';
 
 var request = require('request-promise-native');
 
