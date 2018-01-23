f4e09099f47001d8b88ef4c4932cf7793f7a34c0
Mary Thielking <mthielki@mit.edu>
2018-01-23 13:43:38 -0500

    Event List Component implemented

---
+++ react/Components/EventList.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router';
+
+class EventList extends Component {
+  constructor(props){
+    super(props);
+    this.renderEvent = this.renderEvent.bind(this);
+  }
+
+  renderEvent(e) {
+    const { name, _id, starttime, gid } = e;
+
+    const localStart = new Date(starttime).toString()
+    const date = localStart.substring(4,10)
+      + ", " + localStart.substring(11,15)
+      + " at " + localStart.substring(16,21);
+
+    return (<Link
+      to={`/map/${gid}/event/${_id}/${window.location.search}`}
+      className="list-item-event"
+      key={_id}
+    >
+      <div className="event-name">{name}</div>
+      <div className="event-date">{date}</div>
+    </Link>);
+  }
+
+  render(){
+    const { events } = this.props;
+
+    if (events.length === 0) {
+      return null;
+    }
+    return (<div className="list-items">
+      {events.map((e) => this.renderEvent(e))}
+    </div>);
+  }
+}
+
+EventList.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.object),
+}
+
+EventList.defaultProps = {
+}
+
+export default withRouter(EventList);
