c531c511ced53a1e2b29316bb01eb6c8937c293f
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-25 14:01:41 -0500

    added tag services

--- services/index.js
+++ services/index.js
@@ -2,10 +2,12 @@ import EventServices from './eventServices.js';
 import GreenspaceServices from './greenspaceServices';
 import ReviewServices from './reviewServices';
 import UserServices from './userServices';
+import TagServices from './tagServices';
 
 export default {
     event: EventServices,
     greenspace: GreenspaceServices,
     review: ReviewServices,
     user: UserServices,
+    tag: TagServices,
 }
---
+++ services/tagServices.js
@@ -0,0 +1,26 @@
+const BASE_URL = 'http://localhost:3000/tag'
+// const BASE_URL = 'https://greenspace2018.herokuapp.com/tag';
+
+var request = require('request-promise-native');
+
+export default {
+
+  search : (name) => {
+    return request({
+      uri : BASE_URL + `/${name}`,
+      method: 'GET',
+      json : true
+    });
+  },
+
+  create : (name) => {
+    return request({
+      uri : BASE_URL,
+      method: 'POST',
+      json : true,
+      body : {
+        name,
+      }
+    });
+  },
+}
