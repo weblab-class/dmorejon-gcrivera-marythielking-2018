f8d74b31394817eade7913820479044dcc6539e1
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-29 14:27:25 -0500

    working on landing page

--- react/Pages/Homepage.jsx
+++ react/Pages/Homepage.jsx
@@ -9,23 +9,72 @@ class Homepage extends Component {
   constructor(props){
     super(props);
 
+    this.state = {
+      gifs: [
+       "/images/small_create_greenspace.gif",
+       "/images/small_create_event_4.gif"
+      ],
+      text: [
+        "Create greenspaces by marking outdoor areas!",
+        "Plan events on greenspaces and invite your friends!",
+        "Personalize your discover page to find new natural areas around you!"
+      ],
+      currentGIF: 0,
+    }
+
+    this.goBack = this.goBack.bind(this);
+    this.goForward = this.goForward.bind(this);
+    this.goToGIF = this.goToGIF.bind(this);
+  }
+
+  goBack() {
+    if (this.state.currentGIF === 0) {
+      this.setState({ currentGIF: this.state.gifs.length - 1 });
+    } else {
+      this.setState({ currentGIF: this.state.currentGIF - 1 });
+    }
+  }
+
+  goForward() {
+    if (this.state.currentGIF === this.state.gifs.length - 1) {
+      this.setState({ currentGIF: 0 });
+    } else {
+      this.setState({ currentGIF: this.state.currentGIF + 1 });
+    }
+  }
+
+  goToGIF(e) {
+    this.setState({ currentGIF: parseInt(e.target.id) });
   }
 
   render(){
 
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
+    const {
+      gifs,
+      text,
+      currentGIF,
+    } = this.state;
 
-    // <div id='blurb'>
-    //   Greenspace allows you to mark outdoor areas that other users can view.
-    //    Plan events on these 'greenspaces' (add your friends!), write reviews,
-    //    and find new natural areas around you to check out!
-    // </div>
+    let imgs = [];
+    let circles = [];
+    for (let i = 0; i < gifs.length; i++) {
+      if (i === currentGIF) {
+        imgs.push(<img src={gifs[i]} key={i} className="tutorial-gif show"></img>);
+        circles.push(
+          <div className="circle-container" key={i} onClick={this.goToGIF}>
+            <FontAwesome name="circle" id={i}/>
+          </div>
+        );
+      } else {
+        imgs.push(<img src={gifs[i]} key={i} className="tutorial-gif"></img>);
+        circles.push(
+          <div className="circle-container" key={i} onClick={this.goToGIF}>
+            <FontAwesome name="circle-thin" id={i}/>
+          </div>
+        );
+      }
+    }
+
     let fbLogIn;
     if (!this.props.currentUser) {
       fbLogIn = (
@@ -41,10 +90,31 @@ class Homepage extends Component {
 
     return (
       <PopUp setMapViewOnly={this.props.setMapViewOnly}>
-        <h1>Welcome to Greenspace!</h1>
+        <h1 style={{"marginBottom": "10px", "marginTop": "10px"}}>Welcome to Greenspace!</h1>
           <div id="tutorial-container">
-            <img src="/images/small_create_greenspace.gif" className="tutorial-gif"></img>
-            <img src="/images/small_create_event_4.gif" className="tutorial-gif"></img>
+            <div className="carousel-btn" id="carousel-back" onClick={this.goBack}>
+              <FontAwesome
+                name="angle-left"
+                size="3x"
+                title="Back"
+              />
+            </div>
+            <div id="carousel-imgs">
+              {imgs}
+            </div>
+            <div className="carousel-btn" id="carousel-forward" onClick={this.goForward}>
+              <FontAwesome
+                name="angle-right"
+                size="3x"
+                title="Forward"
+              />
+            </div>
+          </div>
+          <div id="circles-container">
+            {circles}
+          </div>
+          <div>
+            {text[currentGIF]}
           </div>
         <div>
           <a href={`/loading/${window.location.search}`}>
