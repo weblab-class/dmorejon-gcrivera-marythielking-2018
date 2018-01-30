995697a052e5c67b56e0bfd9ac188c970792f450
Gabrielle Rivera <gcrivera@mit.edu>
2018-01-30 04:01:58 -0500

    local -> heroku

--- services/discoverServices.js
+++ services/discoverServices.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:3000/discover'
-// const BASE_URL = 'https://greenspace2018.herokuapp.com/discover';
+// const BASE_URL = 'http://localhost:3000/discover'
+const BASE_URL = 'https://greenspace2018.herokuapp.com/discover';
 
 var request = require('request-promise-native');
 
